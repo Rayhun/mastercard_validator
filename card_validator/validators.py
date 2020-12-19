3273eeaa16025ec90b794b0cf761d803af70ec8d
Rayhun <rayhunkhan27@gmail.com>
2020-12-20 00:12:20 +0600

    add americam express card validator

--- card_validator/validators.py
+++ card_validator/validators.py
@@ -2,7 +2,7 @@ def get_issuer(number: str ) -> str:
     if number.startswith('4'):
         return "Visha"
     
-    if number.startswith('3'):
+    if number.startswith('37') or number.startswith('56'):
         return "American Express"
     
     if number.startswith('51') or number.startswith('52') or number.startswith('53') or number.startswith(
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -6,3 +6,7 @@ def test_get_issuer_visa():
 def test_get_issuer_master():
     assert get_issuer("5456 4567 7654 4567") == "MasterCard"
     assert not get_issuer("5665 6567 6547 9876") == "MasterCard"
+
+def test_get_issuer_american_express():
+    assert get_issuer("378 2822 4631 0005") == "American Express"
+    assert get_issuer("5610 5910 8101 8250") == "American Express"
