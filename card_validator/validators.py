fbc5f9cc4790bf38072838b9d8fec5a03faff898
Rayhun <rayhunkhan27@gmail.com>
2020-12-20 00:05:06 +0600

    add mastercard validator

--- card_validator/validators.py
+++ card_validator/validators.py
@@ -1,9 +1,10 @@
-def get_issuer(number: str) -> str:
+def get_issuer(number: str ) -> str:
     if number.startswith('4'):
         return "Visha"
     
     if number.startswith('3'):
-        return "Amirican Express"
+        return "American Express"
     
-    if number.startswith('5'):
-        return "Master Card"
+    if number.startswith('51') or number.startswith('52') or number.startswith('53') or number.startswith(
+        '54') or number.startswith('55'):
+        return "MasterCard"
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,3 +2,7 @@ from card_validator.validators import get_issuer
 
 def test_get_issuer_visa():
     assert get_issuer("4456 4567 7654 4567") == "Visha"
+
+def test_get_issuer_master():
+    assert get_issuer("5456 4567 7654 4567") == "MasterCard"
+    assert not get_issuer("5665 6567 6547 9876") == "MasterCard"
